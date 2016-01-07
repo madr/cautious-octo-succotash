58d0f428b9a3e039e8f980fdd64116e5c69119ac
Anders Ytterström <yttan@fastmail.se>
2016-01-07 15:33:23 +0100

    adding progresses and projects to router

--- httpapi/views.py
+++ httpapi/views.py
@@ -2,29 +2,49 @@ from django.contrib.auth.models import User, Group
 from oauth2_provider.ext.rest_framework import TokenHasScope
 from rest_framework import permissions, routers, serializers, viewsets
 
+from core.models import Progress, Project
+
+
+class ProgressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Progress
+        #exclude = ('customer',)
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        #exclude = ('customer',)
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        exclude = ('url', 'is_staff')
 
 
-class GroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Group
+class ProjectViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
+    required_scopes = ['reporter']
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ProgressViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
+    required_scopes = ['reporter']
+    queryset = Progress.objects.all()
+    serializer_class = ProgressSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, TokenHasScope, permissions.IsAdminUser]
+    required_scopes = ['management']
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated, TokenHasScope, permissions.IsAdminUser]
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-
-
 httpapi_router = routers.DefaultRouter()
+httpapi_router.register(r'projects', ProjectViewSet)
+httpapi_router.register(r'progresses', ProgressViewSet)
 httpapi_router.register(r'users', UserViewSet)
-httpapi_router.register(r'groups', GroupViewSet)
