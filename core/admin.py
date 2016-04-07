52358260d850d10c49ebc95e331553c46f12573b
Anders Ytterström <yttan@fastmail.se>
2016-04-07 16:12:48 +0200

    adding django admin filters and displays

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from core.models import Project, Progress
 
 
-@admin.register(Progress, Project)
-class SemirhageAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Progress)
+class ProgressAdmin(admin.ModelAdmin):
+    list_display = ['duration', 'note']
+    list_filter = ['user']
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['billable', 'name']
+    list_filter = ['active', 'billable']
+
