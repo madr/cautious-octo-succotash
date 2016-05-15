5bfc792bf8e76fea04a4ca4def9242b25ca75e44
Anders Ytterström <yttan@fastmail.se>
2016-05-15 13:50:58 +0200

    refactoring httpapi

---
+++ httpapi/lib/serializers.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from core.models import Project, Absentia, Progress
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ('password', 'last_login', 'is_staff', 'date_joined', 'first_name',
+                   'last_name', 'groups', 'user_permissions')
+
+
+class AbsentiaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Absentia
+
+
+class ProgressSerializer(serializers.ModelSerializer):
+    included_serializers = {
+        'project': ProjectSerializer,
+        'user': UserSerializer,
+    }
+
+    class Meta:
+        model = Progress
+
+
+class AbsenceSerializer(serializers.ModelSerializer):
+    included_serializers = {
+        'absentia': AbsentiaSerializer,
+        'user': UserSerializer,
+    }
+
+    class Meta:
+        model = Progress
+
+
+class WhoAmISerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    login = serializers.CharField()
+    name = serializers.CharField()
