9853839a3466125f1e2abd00c87c35d5a444ca0b
Anders Ytterström <yttan@fastmail.se>
2016-07-11 02:10:04 +0200

    yep, django defaults messed this up. gg

--- tajmme/settings.py
+++ tajmme/settings.py
@@ -73,10 +73,6 @@ TEMPLATES = [
         'DIRS': [os.path.join(BASE_DIR, 'core/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
-            #'loaders': [
-            #    'django.template.loaders.filesystem.Loader',
-            #    'django.template.loaders.app_directories.Loader',
-            #],
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
