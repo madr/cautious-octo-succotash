fed2ec46c271e25ea32dc9ceda4f2f9aa5aa12e3
Anders Ytterström <yttan@fastmail.se>
2016-07-15 21:20:17 +0200

    changing urls for graphs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,7 +6,7 @@ from dashboard.views import ProgressSearchView
 
 urls = [
     url(r'^$', views.dashboard, name='dashboard'),
-    url(r'^chart-data/bar-chart.js$', views.time_comparison_bar_chart_data, name='bar_chart_data'),
-    url(r'^chart-data/horizontal-bar-chart.js$', views.projects_bar_chart_data, name='hbar_chart_data'),
+    url(r'^chartsjs/week_chart.js$', views.time_comparison_bar_chart_data, name='week_chart_js'),
+    url(r'^chartsjs/projects_chart.js$', views.projects_bar_chart_data, name='projects_chart_js'),
     url(r'^search/$', ProgressSearchView(form_class=ProgressSearchForm), name='search_progresses'),
 ]
