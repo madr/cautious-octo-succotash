f5ed8f481f188178acfbbcfb471d09c3d39a6af9
Anders Ytterström <yttan@fastmail.se>
2016-07-30 20:54:43 +0200

    jit

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,6 +6,7 @@ from dashboard.views import ProgressSearchView
 
 urls = [
     url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^year\/(?P<year>\d+)\/week\/(?P<week_label>\d+)\/summary$', views.week_summary, name='week_summary'),
     url(r'^chartsjs/week_chart.js$', views.time_comparison_bar_chart_data, name='week_chart_js'),
     url(r'^chartsjs/projects_chart.js$', views.projects_bar_chart_data, name='projects_chart_js'),
     url(r'^search/$', ProgressSearchView(form_class=ProgressSearchForm), name='search_progresses'),
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,9 @@
 import datetime
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from haystack.views import FacetedSearchView, SearchView
+from core.lib import TimeUtil
 
 from core.models import Progress, Absence
 from dashboard.lib import get_project_data, get_week_data
@@ -61,3 +62,83 @@ class ProgressSearchView(SearchView):
     def get_context_data(self, *args, **kwargs):
         context = super(ProgressSearchView, self).get_context_data(*args, **kwargs)
         return context
+
+
+@login_required
+def week_summary(request, year, week_label):
+    user = request.user
+
+    year = int(year)
+    week_label = int(week_label)
+
+    week_start, week_end = TimeUtil.week_start_end(year, week_label)
+
+    whole_week_absences = Absence.objects.filter(user=user, done_at__gte=week_start, done_at__lte=week_end)
+    whole_week_progresses = Progress.objects.filter(user=user, done_at__gte=week_start, done_at__lte=week_end)
+
+    ww_absence_count = sum([a.duration for a in whole_week_absences])
+    ww_project_count = len(set([p.project.name for p in whole_week_progresses]))
+    ww_progresses_count = whole_week_progresses.count()
+    ww_minute_count = sum([p.duration for p in whole_week_progresses])
+    ww_billable = sum([p.duration for p in whole_week_progresses.filter(project__billable=True)])
+
+    ww_project_toplist = get_project_data(week_start, week_end, user)
+    ww_summary = get_week_data(week_start, week_end, user)
+
+    try:
+        ww_max_project_toplist_sum = max([v['sum'] for v in ww_project_toplist])
+    except ValueError:
+        ww_max_project_toplist_sum = None
+
+    try:
+        ww_max_project_toplist_count = max([v['count'] for v in ww_project_toplist])
+    except ValueError:
+        ww_max_project_toplist_count = None
+
+    try:
+        ww_max_summary_progresses = max([v['progresses'] for v in ww_summary])
+    except ValueError:
+        ww_max_summary_progresses = None
+
+    try:
+        ww_max_summary_projects = max([v['projects'] for v in ww_summary])
+    except ValueError:
+        ww_max_summary_projects = None
+
+    try:
+        ww_max_summary_sum = max([v['sum'] for v in ww_summary])
+    except ValueError:
+        ww_max_summary_sum = None
+
+    try:
+        ww_billable_pc = int((ww_billable  / float(ww_minute_count)) * 100)
+    except ZeroDivisionError:
+        ww_billable_pc = 0
+
+    context = {
+        'week': week_label,
+        'year': year,
+        'week_start': week_start,
+        'week_end': week_end,
+        'prev_week': TimeUtil.prevweek(year, week_label),
+        'next_week': TimeUtil.nextweek(year, week_label),
+        'spoken_week': TimeUtil.period(year, week_label),
+
+        'ww_absence_count': ww_absence_count,
+        'ww_project_count': ww_project_count,
+        'ww_progresses_count': ww_progresses_count,
+        'ww_minute_count': ww_minute_count,
+        'ww_billable': ww_billable_pc,
+        'ww_nonbillable_count': ww_minute_count - ww_billable,
+        'ww_project_toplist': ww_project_toplist,
+
+        'ww_max_project_toplist_sum': ww_max_project_toplist_sum,
+        'ww_max_project_toplist_count': ww_max_project_toplist_count,
+        'ww_max_summary_progresses': ww_max_summary_progresses,
+        'ww_max_summary_sum': ww_max_summary_sum,
+        'ww_max_summary_projects': ww_max_summary_projects,
+
+        'ww_summary': ww_summary,
+    }
+
+    return render(request, 'week_summary.html', context)
