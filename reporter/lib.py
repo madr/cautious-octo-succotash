5293a27b3f7604e8f359a9cd6e0f0ed7dc75d76a
Anders Ytterström <yttan@fastmail.se>
2016-07-30 20:23:07 +0200

    new design and reporter

--- reporter/lib.py
+++ reporter/lib.py
@@ -228,11 +228,37 @@ def _get_reporter_context(progress_form, user, year, week_label, day):
 
     ww_absence_count = sum([a.duration for a in whole_week_absences])
     ww_project_count = len(set([p.project.name for p in whole_week_progresses]))
+    ww_progresses_count = whole_week_progresses.count()
     ww_minute_count = sum([p.duration for p in whole_week_progresses])
     ww_billable = sum([p.duration for p in whole_week_progresses.filter(project__billable=True)])
 
     ww_project_toplist = get_project_data(week_start, week_end, user)
     ww_summary = get_week_data(week_start, week_end, user)
+
+    try:
+        ww_max_project_toplist_sum = max([v['sum'] for v in ww_project_toplist])
+    except ValueError:
+        ww_max_project_toplist_sum = None
+
+    try:
+        ww_max_project_toplist_count = max([v['count'] for v in ww_project_toplist])
+    except ValueError:
+        ww_max_project_toplist_count = None
+
+    try:
+        ww_max_summary_progresses = max([v['progresses'] for v in ww_summary])
+    except ValueError:
+        ww_max_summary_progresses = None
+
+    try:
+        ww_max_summary_projects = max([v['projects'] for v in ww_summary])
+    except ValueError:
+        ww_max_summary_projects = None
+
+    try:
+        ww_max_summary_sum = max([v['sum'] for v in ww_summary])
+    except ValueError:
+        ww_max_summary_sum = None
 
     try:
         ww_billable_pc = int((ww_billable  / float(ww_minute_count)) * 100)
@@ -257,12 +283,21 @@ def _get_reporter_context(progress_form, user, year, week_label, day):
         'prev_week': TimeUtil.prevweek(year, week_label),
         'next_week': TimeUtil.nextweek(year, week_label),
         'spoken_week': TimeUtil.period(year, week_label),
+
         'ww_absence_count': ww_absence_count,
         'ww_project_count': ww_project_count,
+        'ww_progresses_count': ww_progresses_count,
         'ww_minute_count': ww_minute_count,
         'ww_billable': ww_billable_pc,
         'ww_nonbillable_count': ww_minute_count - ww_billable,
         'ww_project_toplist': ww_project_toplist,
+
+        'ww_max_project_toplist_sum': ww_max_project_toplist_sum,
+        'ww_max_project_toplist_count': ww_max_project_toplist_count,
+        'ww_max_summary_progresses': ww_max_summary_progresses,
+        'ww_max_summary_sum': ww_max_summary_sum,
+        'ww_max_summary_projects': ww_max_summary_projects,
+
         'ww_summary': ww_summary,
     }
 
