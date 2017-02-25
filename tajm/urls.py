7e675c95e1cdec907fd0201ff01bb6d3492602e2
Anders Ytterström <yttan@fastmail.se>
2017-02-25 22:50:47 +0100

    skipping /auth/ prefix on token and authorize endpoints

--- tajm/urls.py
+++ tajm/urls.py
@@ -7,7 +7,7 @@ from tajm.httpapi.views import httpapi_router
 urlpatterns = [
     url(r'^api/v1/', include(httpapi_router.urls)),
     url(r'^admin/', admin.site.urls),
-    url(r'^auth/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    url(r'^', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^id/logout/$', auth.views.logout, {'next_page': '/'}),
     url(r'^id/', include('allauth.urls')),
     url(r'^', include(dashboard.urls)),
