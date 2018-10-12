a0dfc3701cc788f692d94c3204553fc3f299391a
Anders Ytterström <anders.ytterstrom@kundo.se>
2018-10-12 18:02:11 +0200

    Remove apps frontend and docs
    Some frontend remains in the static folder:
    
    * Twitter Bootstrap code for basic ui polish.
    * SVG for logo.

--- frontend/spec/autocompleteSpec.js
+++
@@ -1,6 +0,0 @@
-describe("Autocomplete", function () {
-  it("has all code present", function () {
-    expect(tajmme).not.toBe(undefined);
-    expect(tajmme.Autocomplete).not.toBe(undefined);
-  });
-});
