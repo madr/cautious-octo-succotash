bdf2cea786ad3aeadf065df3fd480a617b08bcf9
Anders Ytterström <anders.ytterstrom@kundo.se>
2018-10-12 18:09:44 +0200

    Remove dashboard app

--- tajm/urls.py
+++ tajm/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin, auth
 
-import tajm.dashboard.urls as dashboard
 from tajm.httpapi.views import httpapi_router
 
 urlpatterns = [
@@ -10,7 +9,6 @@ urlpatterns = [
     url(r'^', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^id/logout/$', auth.views.logout, {'next_page': '/'}),
     url(r'^id/', include('allauth.urls')),
-    url(r'^', include(dashboard.urls)),
 ]
 
 admin.site.site_header = 'Tajm admin'
